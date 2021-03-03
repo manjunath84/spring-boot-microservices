7b81a0982f4576309f9d76ba5b58087ea4fd4350
manjunath84 <manjunath84@gmail.com>
2021-03-03 16:25:23 -0500

    Springboot microservices with Service Discovery using Eureka

---
+++ movie-catalog-service/src/main/java/com/manju/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -0,0 +1,33 @@
+package com.manju.moviecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+
+@SpringBootApplication
+public class MovieCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.setConnectTimeout(Duration.ofMillis(3000)).setReadTimeout(Duration.ofMillis(3000)).build();
+	}
+
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder getWebClient(){
+		return WebClient.builder();
+	}
+
+}
